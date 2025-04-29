039c50c1d7dec9414d8bb37094c9d1c2b149a5ea
Tafajjal Islam <91154086+tafajjal-edu@users.noreply.github.com>
2025-04-29 20:24:00 +0600

    Add files via upload

---
+++ app/privacy/page.tsx
@@ -0,0 +1,10 @@
+import PrivacyPolicy from "@/privacy-policy"
+
+export const metadata = {
+  title: "Privacy Policy",
+  description: "Our privacy policy and how we handle your data",
+}
+
+export default function PrivacyPage() {
+  return <PrivacyPolicy />
+}
